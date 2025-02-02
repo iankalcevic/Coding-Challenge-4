4a87defd91251185bb8ea85713d5409ec4518411
iankalcevic <iankalcevic@usf.edu>
2025-02-02 13:37:40 -0500

    Task 1 - Customer discounts

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+// Task 1 - If statements
+let purchaseAmount = 1000; // Declare purchase amount
+let finalAmount = purchaseAmount; 
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.5; // Discount
+    console.log("Discount applied")
+}
