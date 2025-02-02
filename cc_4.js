2b3cd44b473a05b32d87638663eb2bb7aac580f6
iankalcevic <iankalcevic@usf.edu>
2025-02-02 13:57:19 -0500

    Task 6 - Product Listing

--- cc_4.js
+++ cc_4.js
@@ -38,3 +38,10 @@ let employee = {
 for (let key in employee)
 { console.log('${key}: ${employee[key]}');
 }
+
+// Task 6 - For...Of loop
+let products = ["Blanket", "Pillow", "Couch"]: // Array of products
+for (let product of products) 
+{
+    console.log('Product: ${product}');
+}
